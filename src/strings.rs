721afca1fce0a625662f09b8ea1173f2175b433a
Rajat Jindal <rajatjindal83@gmail.com>
2022-02-21 08:45:26 +0530

    added trim functions

--- src/strings.rs
+++ src/strings.rs
@@ -1,6 +1,4 @@
-use handlebars::{
-    handlebars_helper, Handlebars,
-};
+use handlebars::{handlebars_helper, Handlebars};
 
 pub fn addhelpers(x: &mut Handlebars) {
     handlebars_helper!(upper: |s: String| s.to_uppercase());
@@ -48,6 +46,38 @@ pub fn addhelpers(x: &mut Handlebars) {
         data
     });
 
+    handlebars_helper!(trim_suffix: |suffix: String, input: String| {
+        let result = match input.strip_suffix(suffix.as_str()) {
+            Some(val) => val,
+            _ => input.as_str(),
+        };
+
+        result
+    });
+
+    handlebars_helper!(trim_prefix: |prefix: String, input: String|{
+        let result = match input.strip_suffix(prefix.as_str()) {
+            Some(val) => val,
+            _ => input.as_str(),
+        };
+
+        result
+    });
+
+    handlebars_helper!(trim_all: |substr: String, input: String|{
+        let firstleg = match input.strip_suffix(substr.as_str()) {
+            Some(val) => val,
+            _ => input.as_str(),
+        };
+
+        let result = match firstleg.strip_prefix(substr.as_str()) {
+            Some(val) => val,
+            _ => firstleg,
+        };
+
+        result
+    });
+
     x.register_helper("upper", Box::new(upper));
     x.register_helper("lower", Box::new(lower));
     x.register_helper("trunc", Box::new(trunc));
@@ -58,6 +88,7 @@ pub fn addhelpers(x: &mut Handlebars) {
     x.register_helper("split", Box::new(split));
     x.register_helper("splitn", Box::new(splitn));
     x.register_helper("sort_alpha", Box::new(sort_alpha));
-
-   
+    x.register_helper("trim_suffix", Box::new(trim_suffix));
+    x.register_helper("trim_prefix", Box::new(trim_prefix));
+    x.register_helper("trim_all", Box::new(trim_all));
 }
