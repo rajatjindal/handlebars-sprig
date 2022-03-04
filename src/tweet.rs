865b26d01ded184b362f2ced084875a03dbe6c17
Rajat Jindal <rajatjindal83@gmail.com>
2022-03-04 23:52:36 +0530

    trying tweet

--- src/tweet.rs
+++ src/tweet.rs
@@ -1,18 +1,22 @@
 use super::api::{get_products, FetchResponse};
 use crate::types::Product;
 use handlebars::{handlebars_helper, Handlebars};
+use yew::callback::Callback;
 use yew::format::Json;
 
 //https://github.com/gohugoio/hugo/blob/02d6f5320f4202e2f403151c0aa6902f0a5e3efc/tpl/template_embedded.go
 pub fn addhelpers(x: &mut Handlebars) {
     handlebars_helper!(toJSON: |s: String| {
-        let callback = move |response: FetchResponse<Vec<Product>>| {
+        let callback = Callback::from(|response: FetchResponse<Vec<Product>>| {
             let (_, Json(data)) = response.into_parts();
-            let y = data.unwrap();
-            y
-        };
-        let y = Some(get_products(callback));
-        y
+            match data {
+                Ok(product) => { eprintln!("Internal Server Error: {:?}", product);}
+                Err(e) => { eprintln!("Internal Server Error: {:?}", e);},
+            }
+            
+        });
+        let y = Some(get_products(callback)).unwrap();
+        "xxxxx"
     });
 
     x.register_helper("toJSON", Box::new(toJSON));
